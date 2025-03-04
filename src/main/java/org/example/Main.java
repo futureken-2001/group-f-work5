ba5ecb4c667106a2a2d07b14c45728a709cb182f
Ntale3 <swamaduntale98@gmail.com>
2025-03-04 08:02:32 +0300

    Added JTatoo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,13 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        try{
+        UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
         SwingUtilities.invokeLater(()->{
             new StudentData();
-        });
+        });} catch (RuntimeException | ClassNotFoundException | InstantiationException | IllegalAccessException |
+                    UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
